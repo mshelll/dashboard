d6356f39041c2fd9e6dd996b31221fae207e494b
Marshel <marshel.abraham@gmail.com>
2022-06-12 16:08:49 +0530

    enable typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScoreCard></ScoreCard>
     </div>
   );
+}
+
+
+function ScoreCard() {
+  return(
+    <Card sx={{maxWidth: 50}}>Score</Card>
+  )
 }
 
 export default App;
